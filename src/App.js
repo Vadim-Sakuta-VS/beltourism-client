a9a560aeb4cf4e6b8fa2520901bce42be38f8c58
Vadim <vadim10sakuta@gmail.com>
2020-11-19 23:26:02 +0300

    added local state for Popup component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import './App.scss';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import {Header} from "./components/Header/Header";
 import {Home} from "./components/pages/Home/Home";
 import {Footer} from "./components/Footer/Footer";
+import {Popup} from "./components/Popup/Popup";
 
 function App() {
+  let [popupInfo, setPopupInfo] = useState({activeForm: "", fromFormClosed: ""});
+
   return (
       <Router>
         <div className="app">
-          <Header/>
+          <Popup popupInfo={popupInfo} setPopupInfo={setPopupInfo}/>
+          <Header setPopupInfo={setPopupInfo}/>
           <div className="pages">
             <Switch>
-              <Route render={(props) => <Home {...props} />}/>
+              <Route path="/" render={(props) => <Home {...props} setPopupInfo={setPopupInfo} />}/>
+              <Route path="/home" render={(props) => <Home {...props} />}/>
             </Switch>
           </div>
           <Footer/>

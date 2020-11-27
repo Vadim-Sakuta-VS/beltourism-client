6d4cbabd81d29315415070dbdc00635f1c6f9c46
Vadim <vadim10sakuta@gmail.com>
2020-11-28 00:15:01 +0300

    added new routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {useState} from "react";
 import './App.scss';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
 import {Header} from "./components/Header/Header";
 import {Home} from "./components/pages/Home/Home";
 import {Footer} from "./components/Footer/Footer";
 import {Popup} from "./components/Popup/Popup";
+import Services from "./components/pages/Services/Services";
+import {Page404} from "./components/pages/Page404/Page404";
 
 function App() {
   let [popupInfo, setPopupInfo] = useState({activeForm: "", fromFormClosed: ""});
@@ -16,8 +18,11 @@ function App() {
           <Header setPopupInfo={setPopupInfo}/>
           <div className="pages">
             <Switch>
-              <Route path="/" render={(props) => <Home {...props} setPopupInfo={setPopupInfo} />}/>
-              <Route path="/home" render={(props) => <Home {...props} />}/>
+              <Route exact path="/" render={(props) => <Home {...props} setPopupInfo={setPopupInfo} />}/>
+              <Route exact path="/home" render={(props) => <Home {...props} setPopupInfo={setPopupInfo} />}/>
+              <Route exact path="/services" render={(props) => <Services {...props} setPopupInfo={setPopupInfo}/>}/>
+              <Route path="/page404" render={(props) => <Page404 {...props}/>}/>
+              <Redirect to="/page404"/>
             </Switch>
           </div>
           <Footer/>
